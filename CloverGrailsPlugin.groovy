acf2247540617e8ed179e4b1bd6da3fc8d4f4e62
Nick Pellow <npellow@atlassian.com>
2010-02-17 07:32:50 +0000

    CLOV-776:reformat...

--- CloverGrailsPlugin.groovy
+++ CloverGrailsPlugin.groovy
@@ -1,57 +1,58 @@
-class CloverGrailsPlugin {
-    // the plugin version
-    def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.2.* > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [:]
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp",
-            "samples",
-            "test",
-            "etc/clover.license",
-            "testcases",
-            "docs"
-    ]
+class CloverGrailsPlugin
+{
+  // the plugin version
+  def version = "0.1"
+  // the version or versions of Grails the plugin is designed for
+  def grailsVersion = "1.2.* > *"
+  // the other plugins this plugin depends on
+  def dependsOn = [:]
+  // resources that are excluded from plugin packaging
+  def pluginExcludes = [
+          "grails-app/views/error.gsp",
+          "samples",
+          "test",
+          "etc/clover.license",
+          "testcases",
+          "docs"
+  ]
 
-    def environments = ['test']
+  def environments = ['test']
 
-    // TODO Fill in these fields
-    def author = "Atlassian"
-    def authorEmail = "support@atlassian.com"
-    def title = "Clover for Grails"
-    def description = '''\\
+  // TODO Fill in these fields
+  def author = "Atlassian"
+  def authorEmail = "support@atlassian.com"
+  def title = "Clover for Grails"
+  def description = '''\\
 A plugin that integrates Clover into GRAILS projects.
 '''
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/Clover+Plugin"
+  // URL to the plugin's documentation
+  def documentation = "http://grails.org/Clover+Plugin"
 
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
+  def doWithSpring = {
+    // TODO Implement runtime spring config (optional)
+  }
 
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
+  def doWithApplicationContext = { applicationContext ->
+    // TODO Implement post initialization spring config (optional)
+  }
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional)
-    }
+  def doWithWebDescriptor = { xml ->
+    // TODO Implement additions to web.xml (optional)
+  }
 
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
+  def doWithDynamicMethods = { ctx ->
+    // TODO Implement registering dynamic methods to classes (optional)
+  }
 
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
+  def onChange = { event ->
+    // TODO Implement code that is executed when any artefact that this plugin is
+    // watching is modified and reloaded. The event contains: event.source,
+    // event.application, event.manager, event.ctx, and event.plugin.
+  }
 
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
+  def onConfigChange = { event ->
+    // TODO Implement code that is executed when the project configuration changes.
+    // The event is the same as for 'onChange'.
+  }
 }
