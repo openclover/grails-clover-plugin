be179ec2c407091c61ca4a0e2903ba695cdd1381
Nick Pellow <npellow@atlassian.com>
2010-02-17 07:31:57 +0000

    CLOV-776: set the report title. launch html report  in browser.

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -4,52 +4,55 @@ import org.apache.tools.ant.Project
 import com.cenqua.clover.tasks.AntInstrumentationConfig
 
 
-private static class Defaults {
+def cloverSrcDirs = ["src/java", "src/groovy", "test", "grails-app"];
+def cloverIncludes = ["**/*.groovy", "**/*.java"];
+def cloverExcludes = ["**/conf/**", "**/plugins/**"];
+def cloverReportDir = "${projectWorkDir}/clover/report"
+def cloverHistoryDir = "${basedir}/.cloverhistory"
+def cloverReportTitle = metadata["app.name"]
 
-  static def srcDirs = ["src/java", "src/groovy", "test", "grails-app"];
-  static def includes = ["**/*.groovy", "**/*.java"];
-  static def excludes = ["**/conf/**", "**/plugins/**"];
-  
-}
+println "AppName: ${cloverReportTitle}"
+
 
 eventCompileStart = {kind ->
   ConfigObject config = mergeConfig()
   // Ants Project is available via: kind.ant.project
   println "Clover: Compile start."
-
-//  binding.variables.each { println "${it.key} ${it.value}"} // dumps all available vars and their values
-
+  binding.variables.each { println it.key + " = " + it.value } // dumps all available vars and their values
   System.setProperty "grover.ast.dump", "" + config.dumpAST
-
 }
 
 eventSetClasspath = {URLClassLoader rootLoader ->
 
 //  grailsSettings.compileDependencies.each { println it }
-  
+
   ConfigObject config = mergeConfig()
   println "Clover: Using config: ${config}"
 
   toggleAntLogging(config)
 
-  if (config.enabled) {
+  if (config.enabled)
+  {
 
     toggleCloverOn(config)
 
-    if (!config.containsKey('forceClean') || config.forceClean) {
-          // force a clean
-          def webInf = "${basedir}/web-app/WEB-INF"
-          def cleanDirs = ["${webInf}/classes", "${webInf}/lib", "${projectWorkDir}/gspcompile", classesDirPath, testDirPath, "${projectWorkDir}/clover"]  
+    if (!config.containsKey('forceClean') || config.forceClean)
+    {
+      // force a clean
+      def webInf = "${basedir}/web-app/WEB-INF"
+      def cleanDirs = ["${webInf}/classes", "${webInf}/lib", "${projectWorkDir}/gspcompile", classesDirPath, testDirPath, "${projectWorkDir}/clover"]
 
-          println "Clover: Forcing a clean to ensure Clover instrumentation occurs. Disable by setting: clover.forceClean=false "
-          cleanDirs.each {ant.delete(dir:it, failonerror:false)}
+      println "Clover: Forcing a clean to ensure Clover instrumentation occurs. Disable by setting: clover.forceClean=false "
+      cleanDirs.each {ant.delete(dir: it, failonerror: false)}
 
     }
   }
 }
 
 // copied from $GRAILS_HOME/scripts/_GrailsClean.groovy
-private def cleanCompiledSources() {
+
+private def cleanCompiledSources()
+{
 
 }
 
@@ -65,16 +68,47 @@ eventTestPhasesEnd = {
   ConfigObject config = mergeConfig()
   println "Clover: Tests ended"
 
-  if (!config.enabled) {
+  if (!config.enabled)
+  {
     return;
   }
 
-  // TODO: save a history point?
+  if (!config.historypointtask)
+  {
+    ant.'clover-historypoint'(historyDir: config.historydir ?: cloverHistoryDir)
+  }
+  else
+  {
+    config.historypointtask(ant, binding)
+  }
 
-  if (!config.reporttask) {
+  if (!config.reporttask)
+  {
     // generate a report
-    ant.'clover-html-report'(outdir:config.reportdir ?: "build/clover/report")
-  } else {
+    final GString reportLocation = config.reportdir ?: cloverReportDir
+    ant.'clover-html-report'(outdir: reportLocation,
+            historyDir: config.historydir ?: cloverHistoryDir,
+            title: config.title ?: cloverReportTitle)
+
+    File openFile = new File(reportLocation, "index.html")
+    if (openFile.exists())
+    {
+      if (testNames.size() > 0)
+      {
+        // org/grails/samples/OwnerControllerTests.html
+        File testFile = new File(reportLocation, testNames[0].replaceAll("\\.", File.separator) + "Tests.html")
+        openFile = testFile.exists() ? testFile : openFile
+      }
+
+
+      String openLoc = openFile.toURI().toString()
+      println "About to launch!! ${openLoc}"
+      BareBonesBrowserLaunch.openURL(openLoc);
+    }
+
+  }
+  else
+  {
 
     // reporttask is a user defined closure that takes a single parameter that is a reference to the org.codehaus.gant.GantBuilder instance.
     // this closure can be used to generate a custom html report.
@@ -82,12 +116,14 @@ eventTestPhasesEnd = {
     config.reporttask(ant, binding)
   }
 
+
 }
 
-private def toggleCloverOn(ConfigObject clover) {
+private def toggleCloverOn(ConfigObject clover)
+{
 
   configureLicense(clover)
-  
+
   ant.taskdef(resource: 'cloverlib.xml')
   ant.'clover-env'()
 
@@ -96,42 +132,46 @@ private def toggleCloverOn(ConfigObject clover) {
   configureAntInstr(clover, antConfig)
   antConfig.setIn ant.project
 
-  if (clover.setuptask) {
+  if (clover.setuptask)
+  {
     println "Using custom clover-setup configuration."
 
     clover.setuptask(ant, binding)
-  } else {
+  }
+  else
+  {
     println "Using default clover-setup configuration."
 
     final String initString = clover.get("initstring") != null ? clover.initstring : "${projectWorkDir}/clover/db/clover.db"
     antConfig.initstring = initString
-    
-    srcDirs = clover.srcDirs ? clover.srcDirs: Defaults.srcDirs
-    includes = clover.includes ? clover.includes: Defaults.includes
-    excludes = clover.excludes ? clover.excludes: Defaults.excludes
+
+    cloverSrcDirs = clover.srcDirs ? clover.srcDirs : Defaults.srcDirs
+    cloverIncludes = clover.includes ? clover.includes : Defaults.includes
+    cloverExcludes = clover.excludes ? clover.excludes : Defaults.excludes
 
     println """Clover:
-               directories: ${srcDirs}
-               includes:    ${includes}
-               excludes     ${excludes}"""
+               directories: ${cloverSrcDirs}
+               includes:    ${cloverIncludes}
+               excludes     ${cloverExcludes}"""
 
     ant.'clover-setup'(initString: initString, tmpDir: "${projectWorkDir}/clover/tmp") {
 
-        srcDirs.each {dir ->
-          ant.fileset(dir: dir) {
-            excludes.each { exclude(name: it) }
-            includes.each { include(name: it) }
-          }
+      cloverSrcDirs.each {dir ->
+        ant.fileset(dir: dir) {
+          cloverExcludes.each { exclude(name: it) }
+          cloverIncludes.each { include(name: it) }
         }
+      }
     }
-    
+
   }
 }
 
 /**
  * Takes any CLI arguments and merges them with any configuration defined in BuildConfig.groovy in the clover block.
  */
-private def ConfigObject mergeConfig() {
+private def ConfigObject mergeConfig()
+{
 
   final Map argsMap = parseArguments()
   final ConfigObject config = buildConfig.clover == null ? new ConfigObject() : buildConfig.clover
@@ -154,17 +194,20 @@ private def ConfigObject mergeConfig() {
  * As are String.
  *
  */
-private def configureAntInstr(ConfigObject clover, AntInstrumentationConfig antConfig) {
+private def configureAntInstr(ConfigObject clover, AntInstrumentationConfig antConfig)
+{
 
-   return clover.each {
+  return clover.each {
 
-    if (antConfig.getProperties().containsKey(it.key)) {
+    if (antConfig.getProperties().containsKey(it.key))
+    {
 
       String setter = MetaProperty.getSetterName(it.key)
       MetaProperty property = antConfig.metaClass.getMetaProperty(it.key.toString())
 
       final def val;
-      switch (property.type) {
+      switch (property.type)
+      {
         case Integer.class.getPrimitiveClass("int"):
           val = it.value.toInteger()
           break;
@@ -180,13 +223,14 @@ private def configureAntInstr(ConfigObject clover, AntInstrumentationConfig antC
         default:
           val = it.value
       }
-      
+
       antConfig.invokeMethod(setter, val)
     }
   }
 }
 
-private def configureLicense(ConfigObject clover) {
+private def configureLicense(ConfigObject clover)
+{
 // the directories to search for a clover.license file
   final String[] licenseSearchPaths = ["${userHome}", "${basedir}", "${basedir}/etc", "${grailsWorkDir}"]
 
@@ -194,33 +238,43 @@ private def configureLicense(ConfigObject clover) {
   final LICENSE_PROP = 'clover.license.path'
 
   final license;
-  if (clover.license.path) {
+  if (clover.license.path)
+  {
     license = clover.license.path
-  } else {
+  }
+  else
+  {
 
     licenseSearchPaths.each {
       final String licensePath = "${it}/clover.license"
-      if (new File(licensePath).exists()) {
+      if (new File(licensePath).exists())
+      {
         license = licensePath;
         return;
       }
     }
   }
 
-  if (!license) {
+  if (!license)
+  {
     println """No clover.license configured. Please define license.path=/path/to/clover.license in the
                clover configuration in conf/BuildConfig.groovy"""
-  } else {
+  }
+  else
+  {
     System.setProperty LICENSE_PROP, license
     println "Using clover license path: ${System.getProperty LICENSE_PROP}"
   }
 }
 
-private void toggleAntLogging(ConfigObject clover) {
+private void toggleAntLogging(ConfigObject clover)
+{
 // get any BuildListeners and turn logging on
-  if (clover.debug) {
+  if (clover.debug)
+  {
     ant.project.buildListeners.each {listener ->
-      if (listener instanceof BuildLogger) {
+      if (listener instanceof BuildLogger)
+      {
         listener.messageOutputLevel = Project.MSG_DEBUG
       }
     }
@@ -230,7 +284,9 @@ private void toggleAntLogging(ConfigObject clover) {
 // Copied from _GrailsArgParsing.groovy since _GrailsCompile.groovy does not depend on parseArguments target
 // and the argsMap is not populated in time for the testStart event.
 // see: http://jira.codehaus.org/browse/GRAILS-2663
-private Map parseArguments() {
+
+private Map parseArguments()
+{
   // Only ever parse the arguments once. We also don't bother parsing
   // the arguments if the "args" string is empty.
 //    if (argsMap.size() > 1 || argsMap["params"] || !args) return
@@ -238,22 +294,27 @@ private Map parseArguments() {
 
   args?.tokenize().each {token ->
     def nameValueSwitch = token =~ "--?(.*)=(.*)"
-    if (nameValueSwitch.matches()) { // this token is a name/value pair (ex: --foo=bar or -z=qux)
+    if (nameValueSwitch.matches())
+    { // this token is a name/value pair (ex: --foo=bar or -z=qux)
       final def value = nameValueSwitch[0][2]
-      argsMap[nameValueSwitch[0][1]] = "false".equalsIgnoreCase(value) ? false : value; 
+      argsMap[nameValueSwitch[0][1]] = "false".equalsIgnoreCase(value) ? false : value;
     }
-    else {
+    else
+    {
       def nameOnlySwitch = token =~ "--?(.*)"
-      if (nameOnlySwitch.matches()) {  // this token is just a switch (ex: -force or --help)
+      if (nameOnlySwitch.matches())
+      {  // this token is just a switch (ex: -force or --help)
         argsMap[nameOnlySwitch[0][1]] = true
       }
-      else { // single item tokens, append in order to an array of params
+      else
+      { // single item tokens, append in order to an array of params
         argsMap["params"] << token
       }
     }
   }
 
-  if (argsMap.containsKey('non-interactive')) {
+  if (argsMap.containsKey('non-interactive'))
+  {
     println "Setting non-interactive mode"
     isInteractive = !(argsMap.'non-interactive')
   }
