0263f27d013da8c7e1e94d5c74b2a7c736acd210
Nick Pellow <npellow@atlassian.com>
2010-02-25 02:55:22 +0000

    NONE: bumping release num to m3.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -4,7 +4,7 @@ grails.project.test.reports.dir="build/test"
 clover {
   debug = false;
   on = false;
-  core.version = 'com.cenqua.clover:clover:3.0.0-SNAPSHOT';
+  core.version = 'com.cenqua.clover:clover:3.0.0-m3-SNAPSHOT';
   license.path = "clover.license"
 }
 
