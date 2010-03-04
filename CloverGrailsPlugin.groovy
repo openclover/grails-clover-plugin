f6703715406fbd4d80d6969e9e6bad0b5621f336
Nick Pellow <npellow@atlassian.com>
2010-03-04 02:57:13 +0000

    CLOV-776:added new description to grails plugin

--- CloverGrailsPlugin.groovy
+++ CloverGrailsPlugin.groovy
@@ -21,9 +21,16 @@ class CloverGrailsPlugin
   // TODO Fill in these fields
   def author = "Atlassian"
   def authorEmail = "support@atlassian.com"
-  def title = "Clover for Grails"
+  def title = "Clover Code Coverage for Grails"
   def description = '''\\
 A plugin that integrates Clover into GRAILS projects.
+Clover is a Code Coverage tool that produces beautiful reports which are highly configurable and extensible.
+Clover not only reports which lines of code were executed during a test run, it also reports which tests hit which lines of code.
+Clover's reports include:
+* Tag Clouds of your entire project that compare Complexity with Coverage
+* A TreeMap to visualise the most complex, uncovered areas of your project
+* Fully Cross Referenced HTML view of your source code
+* Much more: See http://atlassian.com/clover 
 '''
 
   // URL to the plugin's documentation
