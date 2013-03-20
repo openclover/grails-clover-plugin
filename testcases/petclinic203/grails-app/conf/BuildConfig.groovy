575f14d0a6383241f646bd3159f0c18471b3e7fa
Marek Parfianowicz <mparfianowicz@atlassian.com>
2013-03-20 13:14:56 +0100

    CLOV-1189 / CLOV-1255: added sample how to use SHARED coverage recorder

--- testcases/petclinic203/grails-app/conf/BuildConfig.groovy
+++ testcases/petclinic203/grails-app/conf/BuildConfig.groovy
@@ -22,4 +22,29 @@ grails.project.dependency.resolution = {
 
 clover {
     license.path="$userHome/clover.license"
+
+    setuptask = { ant, binding, plugin ->
+        ant.delete(dir: "target/clover/db")
+        ant.delete(dir: "target/clover/tmp")
+        ant.'clover-setup'(initstring: "target/clover/db/clover.db",
+                tmpDir: "target/clover/tmp", source : "1.6") {
+            ant.profiles {
+                ant.profile(name: "default", coverageRecorder: "SHARED")
+            }
+        }
+    }
+
+    reporttask =  { ant, binding, plugin ->
+        ant.delete(dir: "target/clover/report")
+        ant.'clover-report'(initstring: "target/clover/db/clover.db") {
+            ant.current(outfile: "target/clover/report/clover.xml") {
+                format(type: "xml")
+            }
+            ant.current(outfile: "target/clover/report") {
+                format(type: "html")
+            }
+        }
+    }
+
 }
+
