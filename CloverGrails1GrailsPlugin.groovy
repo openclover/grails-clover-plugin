0bdac7cc65cf5084f28c41188b971a4115900a2d
Marek Parfianowicz <mparfianowicz@atlassian.com>
2013-10-18 21:25:12 +0200

    NONE: Branched Clover for Grails 1.x and renamed plugin to 'clover-grails1'; updated dependencies in test cases;

--- CloverGrails1GrailsPlugin.groovy
+++ CloverGrails1GrailsPlugin.groovy
@@ -1,4 +1,4 @@
-class CloverGrailsPlugin
+class CloverGrails1GrailsPlugin
 {
   // the plugin version
   def version = "3.2.0"
@@ -20,20 +20,22 @@ class CloverGrailsPlugin
 
   def author = "Atlassian"
   def authorEmail = "support@atlassian.com"
-  def title = "Clover Code Coverage for Grails"
+  def title = "Clover Code Coverage for Grails 1.x"
   def description = '''\\
-A plugin that integrates Clover into GRAILS projects.
+A plugin that integrates Clover into GRAILS 1.x projects.
 Clover is a Code Coverage tool that produces beautiful reports which are highly configurable and extensible.
 Clover not only reports which lines of code were executed during a test run, it also reports which tests hit which lines of code.
 Clover's reports include:
 * Tag Clouds of your entire project that compare Complexity with Coverage
 * A TreeMap to visualise the most complex, uncovered areas of your project
 * Fully Cross Referenced HTML view of your source code
-* Much more: See http://atlassian.com/clover 
+* Much more: See http://atlassian.com/clover
+
+See also: Clover Plugin for Grails 2.x (org.grails.plugins:clover)
 '''
 
   // URL to the plugin's documentation
-  def documentation = "http://grails.org/Clover+Plugin"
+  def documentation = "http://grails.org/plugins/clover-grails1"
 
   def doWithSpring = {
 
