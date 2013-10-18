759b3d798a3ee57f4c651a6801b093130d55145d
Marek Parfianowicz <mparfianowicz@atlassian.com>
2013-10-18 20:51:45 +0200

    NONE: Preparation for a new Clover release - increasing version number to 3.2.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,7 +5,7 @@ grails.project.test.reports.dir	= "target/test-reports"
 clover {
     debug = false;
     on = false;
-    core.version = "com.cenqua.clover:clover:3.1.12"
+    core.version = "com.cenqua.clover:clover:3.2.0"
     license.path = "clover.license"
 }
 
