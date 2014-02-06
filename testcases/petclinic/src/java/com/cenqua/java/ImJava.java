f34b583ca49b6307b7de698448eb37ec75daa3c9
Marek Parfianowicz <mparfianowicz@atlassian.com>
2014-02-06 16:23:38 +0100

    Backed out changeset: 65af6dae7848 (rev 146)

---
+++ testcases/petclinic/src/java/com/cenqua/java/ImJava.java
@@ -0,0 +1,15 @@
+package com.cenqua.java;
+
+import com.cenqua.sample.ImGroovy;
+
+public class ImJava
+{
+
+
+    public ImJava()
+    {
+        System.out.println("I'm a Java class. " + true);
+        new ImGroovy().bar();
+        new ImGroovy().foo();
+    }
+}
