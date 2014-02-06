30ee60569b369ef1ef7146105db286f0f2891060
Marek Parfianowicz <mparfianowicz@atlassian.com>
2014-02-06 16:22:42 +0100

    Backed out changeset: 478e87bd9908 (rev 145)

---
+++ testcases/weceem/grails-app/conf/WeceemPluginUrlMappings.groovy
@@ -0,0 +1,50 @@
+class WeceemPluginUrlMappings {
+    static INTERNAL_URI_PREFIXES = [
+        '_weceem',
+        'admin/',
+        'plugins/',
+        'WEB-INF/',
+        'fck',
+        'WeceemFiles/'
+    ]
+    
+    static mappings = {
+
+        "/admin/$action?"(controller: 'portal')
+
+        "/admin/view/$action?/$id?"(controller: 'preview')
+                
+        "/admin/repository/$action?"(controller: 'repository')
+
+        "/admin/repository/preview/$id"(controller: 'repository', action:'preview')
+
+        "/admin/repository/$space/$action?/$id?"(controller: 'repository')
+                
+        "/admin/editor/$action?/$id?"(controller: 'editor')
+        
+        "/admin/administration/synchronization/$action?/$id?"(controller: 'synchronization')
+
+        "/admin/versions/$action?/$id?"(controller: 'version')
+
+        "/admin/space/$action?/$id?"(controller: 'space')
+
+        "/$uri**" {
+            controller = "content"
+            action = "show"
+            constraints {
+                // @todo this is very ugly, clean up
+                uri(validator: { v ->
+                    !WeceemPluginUrlMappings.INTERNAL_URI_PREFIXES.find { pref -> return v?.startsWith(pref) }
+                })
+            }
+        }
+        
+        "/" {
+            controller = "content"
+            action = "show"
+        }
+
+        "403"(view:'/denied')
+        "500"(view:'/error')
+    }
+}
---
+++ testcases/weceem/grails-app/views/layouts/main/_header.gsp
@@ -0,0 +1,26 @@
+<div class="container"> 
+    <div id="adminLogo" class="span-14"></div>
+    
+    <div id="iconbar" class="span-10 last prepend-top" style="text-align: right">
+        <span title="Current User">
+          Welcome, <wcm:loggedInUserName/>
+        </span>
+        <g:link url="${wcm.userProfileEditUrl().encodeAsHTML()}">Profile</g:link> |
+        <a href="http://www.weceem.org/">Help</a> |
+        <g:link url="${wcm.userLogOutUrl().encodeAsHTML()}">Log Out</g:link>
+    </div>
+
+    <div id="navigation" class="span-24 last">
+        <nav:render group="weceem" actionMatch="true"/>
+    </div>
+
+    
+    <g:if test="${flash.message}">
+      <div class="message span-22 prepend-1 append-1 prepent-top append-bottom last ui-state-highlight ui-corner-all">${flash.message.encodeAsHTML()}</div>
+    </g:if>
+    <g:if test="${flash.error}">
+      <div class="message span-22 prepend-1 append-1 prepent-top append-bottom last ui-state-error ui-corner-all">${flash.error}</div>
+    </g:if>
+
+</div>
+
