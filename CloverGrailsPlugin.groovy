fca11c600ce9ceb689fa9dd4d4df290cc1368601
Marek Parfianowicz <mparfianowicz@atlassian.com>
2015-04-20 15:27:52 +0200

    NONE: prepare for next development iteration

--- CloverGrailsPlugin.groovy
+++ CloverGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class CloverGrailsPlugin
 {
-  def version = "4.0.4"
+  def version = "4.0.5"
   def grailsVersion = "1.3.* > *"
   def pluginExcludes = [
           "samples",
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,7 +3,7 @@ grails.project.work.dir = "target"
 clover {
     debug = false
     on = false
-    core.version = "com.atlassian.clover:clover:4.0.4"
+    core.version = "com.atlassian.clover:clover:4.0.5-SNAPSHOT"
     license.path = "clover.license"
 }
 
