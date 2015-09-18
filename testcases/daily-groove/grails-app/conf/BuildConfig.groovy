0924e410173018a660b394f8d498fd7b4f7d21c8
grzegorz lewandowski <glewandowski@atlassian.com>
2015-09-18 14:52:22 +0200

    CLOV-1772: Bump version

--- testcases/daily-groove/grails-app/conf/BuildConfig.groovy
+++ testcases/daily-groove/grails-app/conf/BuildConfig.groovy
@@ -30,7 +30,7 @@ grails.project.dependency.resolution = {
           exclude "resources"
         }
         runtime ":hibernate:1.3.8"
-        build ":clover:4.0.4"
+        build ":clover:4.0.6"
         build ":tomcat:1.3.8"
     }
     dependencies {
